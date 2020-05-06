17bac7c0aec91ff74e071e1d540b8fa4726da368
Jean <jean.soro007@gmail.com>
2020-05-06 12:00:25 -0400

    imported all pages component into App component

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Error from "./pages/Error";
+import SingleCocktail from "./pages/SingleCocktail";
+
+
+export default function App() {
+  return <h1>app component</h1>;
+}
---
+++ src/components/Cocktail.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Cocktail = () => {
+  return <h1>cocktail component</h1>;
+}
+
+export default Cocktail;
