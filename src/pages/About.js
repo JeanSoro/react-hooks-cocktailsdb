7d03a96169629bd0bf1877295cf959c6e164b9b6
Jean <jean.soro007@gmail.com>
2020-05-06 12:30:08 -0400

    Added styles to Error and About Pages Components

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 const About = () => {
-  return <h1>about page</h1>;
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">About Us</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus iste necessitatibus, quidem dignissimos incidunt rem vel atque excepturi nulla ex ab dicta itaque. Sed consequatur pariatur aliquid voluptas non accusamus cum fugiat deserunt possimus ratione.</p>
+    </section>
+  );
 }
 
 
