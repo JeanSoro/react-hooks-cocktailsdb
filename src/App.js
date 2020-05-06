fa5f1f1c73df58ac5dab5244861bf5bb8553473d
Jean <jean.soro007@gmail.com>
2020-05-06 12:14:15 -0400

    React Router setup completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Error from "./pages/Error";
 import SingleCocktail from "./pages/SingleCocktail";
 
 
-export default function App() {
-  return <h1>app component</h1>;
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="*">
+          <Error />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
+
+export default App;
